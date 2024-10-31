c4a19d69afc4fed8415b83923413e090e93d8d9c
Carlos Heredero <herederocarlos9@gmail.com>
2024-10-31 22:24:30 +0800

    fixed address

--- add.php
+++ add.php
@@ -7,6 +7,7 @@
         $job_title = $_POST['job_title'];
         $business_phone = $_POST['business_phone'];
         $fax_number = $_POST['fax_number'];
+        $address = $_POST['address'];
         $city = $_POST['city'];
         $state_province = $_POST['state_province'];
         $zip_postal_code = $_POST['zip_postal_code'];
@@ -16,7 +17,7 @@
         $result = mysqli_query($conn, $sql) or die("Connection Failed.".$conn->connect_error);
 
         if ($result) {
-            header("Location: index.php");
+            header("Location: index2.php");
         }
         else {
             echo "Not successful.";
@@ -63,6 +64,7 @@ $conn->close();
             <label>Job Title:</label><input type="text" name="job_title">
             <label>Business Phone:</label><input type="text" name="business_phone">
             <label>Fax Number:</label><input type="text" name="fax_number">
+            <label>Address:</label><input type="text" name="address">
             <label>City:</label><input type="text" name="city">
             <label>State Province:</label><input type="text" name="state_province">
             <label>Zip Postal Code:</label><input type="text" name="zip_postal_code">
