72d08d712be1c4b9649b439041f1ea59539c1f4b
ArnejoKeinth <202280176@psu.palawan.edu.ph>
2024-10-31 22:54:36 +0800

    fixed address field in add.php

--- add.php
+++ add.php
@@ -7,12 +7,13 @@
         $job_title = $_POST['job_title'];
         $business_phone = $_POST['business_phone'];
         $fax_number = $_POST['fax_number'];
+        $address = $_POST['address'];
         $city = $_POST['city'];
         $state_province = $_POST['state_province'];
         $zip_postal_code = $_POST['zip_postal_code'];
         $country_region = $_POST['country_region'];
 
-        $sql = "INSERT INTO customers (first_name, last_name, job_title, business_phone, fax_number, address, city, state_province, zip_postal_code, country_region) VALUES ('$first_name', '$last_name', '$job_title', '$business_phone', '$fax_number', 'NULL', '$city', '$state_province', '$zip_postal_code', '$country_region')";
+        $sql = "INSERT INTO customers (first_name, last_name, job_title, business_phone, fax_number, address, city, state_province, zip_postal_code, country_region) VALUES ('$first_name', '$last_name', '$job_title', '$business_phone', '$fax_number', '$address', '$city', '$state_province', '$zip_postal_code', '$country_region')";
         $result = mysqli_query($conn, $sql) or die("Connection Failed.".$conn->connect_error);
 
         if ($result) {
@@ -63,6 +64,7 @@ $conn->close();
             <label>Job Title:</label><input type="text" name="job_title">
             <label>Business Phone:</label><input type="text" name="business_phone">
             <label>Fax Number:</label><input type="text" name="fax_number">
+            <label>Address:</label><input type="text" name="address">
             <label>City:</label><input type="text" name="city">
             <label>State Province:</label><input type="text" name="state_province">
             <label>Zip Postal Code:</label><input type="text" name="zip_postal_code">
